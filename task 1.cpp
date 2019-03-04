91eb9a12d32a92c1668c8cf9553d6a6083254365
preethamkumar88 <prithamofficial@gmail.com>
2019-03-04 19:23:39 +0530

    Create task 1.cpp
    programm regarding task 1

---
+++ task 1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	char name [15];
+	int age;
+	int phonenumber;
+	printf("enter name");
+	scanf("%s,&name");
+	printf("enter age");
+	scanf("%d,&age");
+	printf("phone number");
+	scanf("%s,&phone number");
+	printf("nameis %s /n age is %d /n phone number is %s,name,age,phone number");
+}
